473a4955482c0c1d144bd78f0b362eaab4b911f4
Chasi650z <chasi64.z@gmail.com>
2023-10-08 13:31:23 -0500

    pruebas del enrutamiento con rest client

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,11 +1,9 @@
-modeule.exports = app;
-
 const express = require('express')
 const morgan  = require('morgan')
 const app = express()
 
 app.set('port', process.env.PORT || 4000);
-
+module.exports = app;
 
 app.use(morgan('dev'))
 
